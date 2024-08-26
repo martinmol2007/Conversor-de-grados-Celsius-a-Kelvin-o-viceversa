ec953a9aba6b0d920e22f2e493dfb3983c67b3c3
Martin Molina <74263137+martinmol2007@users.noreply.github.com>
2024-08-26 17:01:37 +0200

    [Català] Convertidor de graus Celsius a graus Kelvin.py

--- Conversor de Grados Celcius a Kelvin.py
+++ Conversor de Grados Celcius a Kelvin.py
@@ -1,21 +1,21 @@
-print ("Bienvenido al conversor de grados Celcius a Kelvin o viceversa")
+print("Benvingut al convertidor de graus Celsius a Kelvin o viceversa")
 
 def convertidor_temperatura():
-    print ("¿Que conversion de las siguientes opciones desea hacer?")
-    print ("1: Pasar de Celcius a Kelvin")
-    print ("2: Pasar de Kelvin a Celcius")
+    print("Quina conversió de les següents opcions desitja fer?")
+    print("1: Passar de Celsius a Kelvin")
+    print("2: Passar de Kelvin a Celsius")
 
-    opcion = input("Ingrese una de las dos opciones (1 o 2): ")   
+    opció = input("Introdueix una de les dues opcions (1 o 2): ")
 
-    if opcion == "1":
-      celcius = float(input("Introduce el numero de grados Celcius que deseas convertir a grados Kelvin: "))
-      kelvin = celcius + 273.15
-      print (str(celcius) + " °C son " + str(kelvin) + " K ")
-    elif opcion == "2":
-      kelvin = float(input(" Introduce el numero de grados Kelvin que deseas transformar a grados Celcius: "))
-      celcius = kelvin - 273.15
-      print (str(kelvin) + " K son " + str(celcius) + " °C ")
+    if opció == "1":
+        celsius = float(input("Introdueix el nombre de graus Celsius que desitges convertir a graus Kelvin: "))
+        kelvin = celsius + 273.15
+        print(str(celsius) + " °C són " + str(kelvin) + " K")
+    elif opció == "2":
+        kelvin = float(input("Introdueix el nombre de graus Kelvin que desitges transformar a graus Celsius: "))
+        celsius = kelvin - 273.15
+        print(str(kelvin) + " K són " + str(celsius) + " °C")
     else:
-      print ("Opcion no valida. Vuelve a intentarlo")
+        print("Opció no vàlida. Torna-ho a intentar")
 
 convertidor_temperatura()
